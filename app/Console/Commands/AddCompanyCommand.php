31c544bb58bfde0d305bfc5a2a44cdf1734079a8
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2019-09-22 15:33:01 +0700

    Make Artisan Commands - Part 1

---
+++ app/Console/Commands/AddCompanyCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Company;
+use Illuminate\Console\Command;
+
+class AddCompanyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'contact:company {name} {phone=N/A}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds a new company';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $company = Company::create([
+            'name' => $this->argument('name'),
+            'phone' => $this->argument('phone'),
+        ]);
+
+        $this->info('Added ' . $company->name);
+    }
+}
